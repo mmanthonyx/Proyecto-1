e6f7edf919ab11df3b104a3234650d8d64532835
mmanthonyx <55038436+mmanthonyx@users.noreply.github.com>
2022-08-06 19:25:44 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+      <h2>Estoy iniciando en esto</h2>
+    </div>
+  );
+}
